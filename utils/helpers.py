c50eb79cb036cd97bc67832e500549a49a58dca0
olegkhomenko <deadfrisk@gmail.com>
2020-02-16 00:28:33 +0300

    Most common cities are added

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,12 +1,16 @@
-import twitter
+from typing import Union, Tuple
+
 import pandas as pd
+import twitter
+
+from utils import load_config
 
 
 def get_friends(user_id: int,
                 api: twitter.api.Api,
                 n_friends: int = None,
                 max_requests=100,
-                ):
+                ) -> list:
 
     friends = []
     next_cursor = -1
@@ -39,14 +43,57 @@ def remove_outliers(df: pd.DataFrame):
     return df[mask]
 
 
-class Statistics:
-    def __init__(self, ):
-        pass
+class TWAnalytics:
+    FILE_SYNONYMS = 'data/synonyms.yaml'
+
+    def __init__(self, api: twitter.api.Api):
+        self.api = api
+
+    def get_friends_df(self, user_id: int):
+        return self.friends_to_df(get_friends(user_id, self.api))
+
+    def friends_cities(self,
+                       user_id: int = None,
+                       friends_df: pd.DataFrame = None,
+                       return_friends_df=True,
+                       ) -> Union[dict, Tuple[dict, pd.DataFrame]]:
+
+        if user_id is None and friends_df is None:
+            raise ValueError("Please pass user_id or friends_df as a parameter")
+
+        if friends_df is None:
+            friends_df = self.get_friends_df(user_id)
+            if return_friends_df:
+                friends_df_orig = friends_df.copy()
+
+        friends_df.location = friends_df.location.map(  # Replace different names of cities w/ a common name
+            lambda x: x.split(',')[0] if isinstance(x, str) else x).replace(self.cities)
+
+        friends_cities: pd.Series = friends_df.location.value_counts().sort_values(ascending=False)
+
+        if return_friends_df:
+            return friends_cities.to_dict(), friends_df_orig
+        return friends_cities.to_dict()
+
+    def friends_followers_count_stats(self,
+                                      user_id: int = None,
+                                      friends_df: pd.DataFrame = None,
+                                      ):
+
+        if user_id is None and friends_df is None:
+            raise ValueError("Please pass user_id or friends_df as a parameter")
+
+        if friends_df is None:
+            friends_df = self.get_friends_df(user_id)
+
+        return friends_df.followers_count.agg(['min', 'max', 'median', 'std', 'mean']).to_dict()
 
     @staticmethod
-    def friends_statistics(friends: list):
-        pass
+    def friends_to_df(friends: list) -> pd.DataFrame:
+        return pd.DataFrame({f.id: f.AsDict() for f in friends}).T
 
-class Analytics:
-    @staticmethod
-    def favorit
+    @property
+    def cities(self):
+        if not hasattr(self, '_synonyms'):
+            self._synonyms = load_config(self.FILE_SYNONYMS)
+        return self._synonyms['cities']
