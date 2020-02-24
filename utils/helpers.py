8e4cf9aebbaa857cf7590c101c144ab69622e60b
olegkhomenko <deadfrisk@gmail.com>
2020-02-24 12:43:06 +0300

    fixes

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,9 +1,16 @@
-from typing import Union, Tuple
+import os
+import string
+from typing import Tuple, Union
 
+import gensim
+import nltk
 import pandas as pd
 import twitter
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer, sent_tokenize, word_tokenize
 
 from utils import load_config
+
 
 
 def get_friends(user_id: int,
@@ -46,6 +53,7 @@ def remove_outliers(df: pd.DataFrame):
 class TWAnalytics:
     FILE_SYNONYMS = 'data/synonyms.yaml'
     FILE_LOCATIONS = 'data/locations.yaml'
+    FILE_STOP_WORDS = 'data/stop_words.yaml'
     CONFIG = 'config.yaml'
 
     def __init__(self, api: twitter.api.Api = None, config_path=None):
@@ -55,6 +63,8 @@ class TWAnalytics:
             api = twitter.Api(**api_kwargs)
 
         self.api = api
+        self.check_nltk()
+        self.tweet_tokenizer = TweetTokenizer()
 
     def get_friends_df(self, user_id: int):
         return self.friends_to_df(get_friends(user_id, self.api))
@@ -95,6 +105,29 @@ class TWAnalytics:
 
         return friends_df.followers_count.agg(['min', 'max', 'median', 'std', 'mean']).to_dict()
 
+    def get_tokenized_timeline(self,
+                               user_id: int = None,
+                               ):
+        tweets = self.api.GetUserTimeline(user_id=user_id)
+        tweets_sentences = [sent_tokenize(t.full_text, language='russian') for t in tweets]
+
+        tokenized_tweets = []
+        for tweet in tweets_sentences:
+            tweet_tokenized = []
+            for sent in tweet:
+                tweet_tokenized += [self._tokenize(sent)]
+            tokenized_tweets += [tweet_tokenized]
+
+        return tokenized_tweets
+
+    def _tokenize(self, text: str):
+        tokens = self.tweet_tokenizer.tokenize(text)
+        tokens = [i for i in tokens if (i not in string.punctuation)]
+        stop_words = stopwords.words('russian')
+        stop_words.extend(self.stop_words)
+        tokens = [i for i in tokens if (i not in stop_words)]
+        return tokens
+
     @staticmethod
     def friends_to_df(friends: list) -> pd.DataFrame:
         return pd.DataFrame({f.id: f.AsDict() for f in friends}).T
@@ -111,3 +144,35 @@ class TWAnalytics:
             self._locations = load_config(self.FILE_LOCATIONS)
 
         return self._locations
+
+    @property
+    def stop_words(self):
+        if not hasattr(self, '_stop_words'):
+            self._stop_words = load_config(self.FILE_STOP_WORDS)
+
+        return self._stop_words
+
+    @classmethod
+    def check_nltk(cls):
+        for el in ['tokenizers/punkt', 'corpora/stopwords']:
+            try:
+                nltk.data.find(el)
+            except LookupError:
+                nltk.download(os.path.basename(el))
+
+
+class TWModel:
+    MODEL_PATH = './182.zip'
+    MODEL_URI = 'http://vectors.nlpl.eu/repository/20/182.zip'
+
+    def __init__(self, model_path=None):
+        model_path = self.MODEL_PATH if model_path is None else model_path
+        self.model = gensim.models.Word2Vec.load(model_path)
+
+    def train(self, sentences: list):
+        self.model =
+
+    def _download_weights(self):
+        # TODO: Download using model uri
+        raise NotImplementedError
+    
